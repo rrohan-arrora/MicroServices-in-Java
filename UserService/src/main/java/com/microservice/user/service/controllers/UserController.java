c2ccf79c088b7b4339e0bff333fce5b5876f6761
Aaksh Gadekar <an14092000@gmail.com>
2023-10-10 19:19:50 +0530

    Added controller methods for the user

--- UserService/src/main/java/com/microservice/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/microservice/user/service/controllers/UserController.java
@@ -1,6 +1,54 @@
 package com.microservice.user.service.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.user.service.entities.User;
+import com.microservice.user.service.services.UserService;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	// create a single user
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User user){
+		User user_ = userService.saveUser(user);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(user_);
+	}
+	
+	// get a user
+	@GetMapping("/{userId}")
+	public ResponseEntity<Optional<User>> getSingleUser(@PathVariable String userId){
+		Optional<User> user = userService.getUser(userId);
+        return ResponseEntity.ok(user);
+	}
+	
+	//all user get
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> allUser = userService.getAllUsers();
+        return ResponseEntity.ok(allUser);
+    }
+	
+	
+	
+	
 	
 	
 
