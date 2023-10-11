06fd28f231c4d49012d4f9d36648e642a44e4897
Aaksh Gadekar <an14092000@gmail.com>
2023-10-11 23:21:09 +0530

    Updated the configuration in order to register rating service to eureka

--- RatingService/src/main/java/com/micro/service/RatingService/RatingServiceApplication.java
+++ RatingService/src/main/java/com/micro/service/RatingService/RatingServiceApplication.java
@@ -2,10 +2,12 @@ package com.micro.service.RatingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = { 
 	    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
 	})
+@EnableDiscoveryClient
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
