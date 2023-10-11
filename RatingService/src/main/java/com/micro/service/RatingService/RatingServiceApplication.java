26995237e1cccc65f0e6415bd68ac9db64fc0ad0
Aaksh Gadekar <an14092000@gmail.com>
2023-10-11 17:13:32 +0530

    Added initial configuration for mongodb spring boot projects

---
+++ RatingService/src/main/java/com/micro/service/RatingService/RatingServiceApplication.java
@@ -0,0 +1,16 @@
+package com.micro.service.RatingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = { 
+	    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+	    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+	})
+public class RatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingServiceApplication.class, args);
+	}
+
+}
